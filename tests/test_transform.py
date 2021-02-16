7ddd49627356bbb77b9d9a4e9806067f3a9d5212
Tal Luigi <luigi.t.agr@gmail.com>
2021-02-16 00:23:02 -0500

    feat: created a test suite for the is_valid_date utility function

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,5 +1,5 @@
 # Imports
-from etl.transform import is_valid_phone
+from etl.transform import is_valid_phone, is_valid_date
 
 
 # Tests
@@ -49,5 +49,47 @@ class TestIsValidPhone:
         phone = "1234567890"
         # Act
         result = is_valid_phone(phone)
+        # Assert
+        assert result is True
+
+
+class TestIsValidDate:
+    def test_none(self):
+        # Arrange
+        date = None
+        # Act
+        result = is_valid_date(date)
+        # Assert
+        assert result is False
+
+    def test_empty_string(self):
+        # Arrange
+        date = ""
+        # Act
+        result = is_valid_date(date)
+        # Assert
+        assert result is False
+
+    def test_invalid_default_date(self):
+        # Arrange
+        date = "1900-01-01T00:00:00.000"
+        # Act
+        result = is_valid_date(date)
+        # Assert
+        assert result is False
+
+    def test_partial_date(self):
+        # Arrange
+        date = "2020-01-31"
+        # Act
+        result = is_valid_date(date)
+        # Assert
+        assert result is True
+
+    def test_full_date(self):
+        # Arrange
+        date = "2020-01-31T00:00:00.000"
+        # Act
+        result = is_valid_date(date)
         # Assert
         assert result is True
