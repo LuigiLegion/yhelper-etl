8b17e436ed4e36c6d20814fc5afc8a33daccb590
Tal Luigi <luigi.t.agr@gmail.com>
2021-03-01 22:47:49 -0500

    chore: updated the total inspections count comment in the extract function

--- etl/extract.py
+++ etl/extract.py
@@ -22,7 +22,7 @@ def extract(token: Optional[str], collection: str, document: str, limit: int, ex
         # Extract data from document
         data = client.get(document, limit=limit)
         # Print number of inspections in dataset
-        print("total inspections: ", len(data))  # 397481
+        print("total inspections: ", len(data))  # 396020
 
         with open(extract_file, "w") as f:
             dump(data, f, indent=4)
