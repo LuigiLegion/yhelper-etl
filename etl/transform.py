7e607ede693dd6f4088971149024be44d3143273
Tal Luigi <luigi.t.agr@gmail.com>
2021-04-12 03:34:25 -0400

    feat: created the grades, unix_time, utc_time_object, truncated_decimal, ratio, and statistics utility functions and used them in the transform function to add grade statistics to each restaurant

--- etl/transform.py
+++ etl/transform.py
@@ -1,7 +1,9 @@
 # Imports
 import re
 from operator import itemgetter
-from typing import List, Optional
+from datetime import datetime
+from decimal import Decimal, ROUND_HALF_EVEN
+from typing import List, Optional, Union
 
 from simplejson import load, dump
 
@@ -75,6 +77,68 @@ def formatted_grade(
 
 def formatted_critical(critical: Optional[str]) -> bool:
     return True if critical == "Y" else False
+
+
+def grades() -> dict:
+    return {
+        "A": 0,
+        "B": 0,
+        "C": 0,
+    }
+
+
+def unix_time(utc_time_obj) -> float:
+    return (utc_time_obj - datetime(1970, 1, 1)).total_seconds()
+
+
+def utc_time_object(utc_timestamp: str):
+    return datetime.strptime(utc_timestamp, "%Y-%m-%dT%H:%M:%S.%f")
+
+
+def truncated_decimal(decimal: Union[float, int], scale: int) -> Decimal:
+    dec = Decimal(decimal)
+    exp = Decimal("0." + ("0" * (scale - 1)) + "1")
+
+    return dec.quantize(exp, rounding=ROUND_HALF_EVEN)
+
+
+def ratio(grade_duration: float, total_duration: float) -> Decimal:
+    return truncated_decimal((grade_duration / total_duration) * 100, scale=2)
+
+
+def statistics(sorted_insps: List[dict]) -> dict:
+    durations = grades()
+
+    insps_count = len(sorted_insps) - 1
+    prev_grade = None
+
+    for i, insp in enumerate(sorted_insps):
+        grade = insp.get("grade")
+
+        if grade:
+            if prev_grade is None:
+                prev_grade = grade
+
+            if i == insps_count:
+                insp_unix_time = unix_time(datetime.utcnow())
+                prev_insp_unix_time = unix_time(utc_time_object(insp.get("date")))
+
+            else:
+                insp_unix_time = unix_time(utc_time_object(insp.get("date")))
+                prev_insp_unix_time = unix_time(utc_time_object(sorted_insps[i + 1].get("date")))
+
+            unix_time_diff = insp_unix_time - prev_insp_unix_time
+            durations[grade] += unix_time_diff
+
+    total_duration = durations.get("A") + durations.get("B") + durations.get("C")
+
+    ratios = grades()
+
+    if total_duration:
+        for grade, duration in durations.items():
+            ratios[grade] = ratio(duration, total_duration)
+
+    return ratios
 
 
 def violation(datum: dict) -> dict:
@@ -151,8 +215,10 @@ def transform(extract_file: str, transform_file: str) -> List[dict]:
     rests_list = []
 
     for phone, rest in rests.items():
-        insps_list = [i for i in rest.get("inspections").values()]
+        insps_list = [insp for insp in rest.get("inspections").values()]
         sorted_insps = sorted(insps_list, key=itemgetter("date"), reverse=True)
+
+        rest["statistics"] = statistics(sorted_insps)
 
         for sorted_insp in sorted_insps:
             sorted_insp["date"] = formatted_date(sorted_insp.get("date"))
@@ -161,7 +227,7 @@ def transform(extract_file: str, transform_file: str) -> List[dict]:
         rests_list.append({phone: rest})
 
     # Print number of restaurants with valid inspections in dataset
-    print(f"total restaurants count: {len(rests_list)}")  # 24252
+    print(f"total restaurants count: {len(rests_list)}")  # 24258
 
     # Dump restaurants data to file
     with open(transform_file, "w") as tf:
